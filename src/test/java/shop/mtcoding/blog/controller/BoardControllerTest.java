9d77a3f2bfada06aa449885aa69d2dda9699fa71
hyunseok <dopocoo@naver.com>
2023-02-07 10:35:14 +0900

    게시글 목록보기 통합 test 완료

--- src/test/java/shop/mtcoding/blog/controller/BoardControllerTest.java
+++ src/test/java/shop/mtcoding/blog/controller/BoardControllerTest.java
@@ -1,10 +1,14 @@
 package shop.mtcoding.blog.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +21,9 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.blog.dto.board.BoardResp;
 import shop.mtcoding.blog.model.User;
 
 @AutoConfigureMockMvc
@@ -27,6 +34,9 @@ public class BoardControllerTest {
     private MockMvc mvc;
 
     private MockHttpSession mockSessin;
+
+    @Autowired
+    private ObjectMapper om;
 
     @BeforeEach // Test 메서드 실행 직전 마다에 호출됨
     public void setUp() {
@@ -39,6 +49,25 @@ public class BoardControllerTest {
 
         mockSessin = new MockHttpSession();
         mockSessin.setAttribute("principal", user);
+    }
+
+    @Test
+    public void main_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/"));
+        Map<String, Object> map = resultActions.andReturn().getModelAndView().getModel();
+        List<BoardResp.BoardMainRespDto> dtos = (List<BoardResp.BoardMainRespDto>) map.get("dtos");
+        String responseBody = om.writeValueAsString(dtos);
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        resultActions.andExpect(status().isOk());
+        assertThat(dtos.size()).isEqualTo(6);
+        assertThat(dtos.get(0).getUsername()).isEqualTo("ssar");
+        assertThat(dtos.get(0).getTitle()).isEqualTo("제목1");
     }
 
     @Test
