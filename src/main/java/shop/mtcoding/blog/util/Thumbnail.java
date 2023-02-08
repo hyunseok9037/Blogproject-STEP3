b6d431405feb33fa7e10fb1a00bb96027517962d
hyunseok <dopocoo@naver.com>
2023-02-08 17:17:01 +0900

    util Thumbnail 메서드완료

---
+++ src/main/java/shop/mtcoding/blog/util/Thumbnail.java
@@ -0,0 +1,23 @@
+package shop.mtcoding.blog.util;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Thumbnail {
+
+    public String thum(String html) {
+        String thumbnail = "";
+        Document doc = Jsoup.parse(html);
+        Elements els = doc.select("img");
+        if (els.size() == 0) {
+
+        } else {
+            Element el = els.get(0);
+            thumbnail = el.attr("src");
+        }
+        return thumbnail;
+    }
+
+}
